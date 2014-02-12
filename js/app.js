a5f2b25b9c0ca7afe589aa39680fffa0122cae16
Nicholas Bartlett <nbartlett84@gmail.com>
2014-02-12 07:54:48 -0500

    adding ability to have a single page application that swaps out a middle section for each route within the page

--- js/app.js
+++ js/app.js
@@ -1,53 +1,70 @@
 $(document).ready(function() {
 
-    var main_content_div = $('div.main-content');
-    var all_nav_list_items = $('nav li');
+    // select the main div and cache for easy access
+    var main_content_div = $('div#main-content');
+    var all_nav_list_items = $('.nav li');
 
+    // this is a function that will add the "active" css class to the 
+    // active controller.  this will allow the user to see the tab they are
+    // on as highlighted in the webpage
     var updateNav = function(activeControllers) {
         all_nav_list_items.removeClass("active");
         activeControllers.addClass("active");
     };
 
-    var getRenderer = function(templatePath, activeControllers) {
-        var promise = $.get(templatePath);
-        return function() {
-            // set page content
-            promise.done(function(data){ main_content_div.html(data); });
+    // utilility function that retuns a function which is called to render
+    // html to the main div area.  that html is lazy loaded and cached within
+    // the "promise" object.  once the data has been loaded, calling .done(callback)
+    // just immediately calls the callback on the cached result of the ajax call
+    var getRenderer = function(path, activeNode) {
+        // lazy loaded page
+        var promise = null;
 
-            // update the active status
-            updateNav(activeControllers);
+        return function() {
+            // updates the nav bar to indicate which tab is active
+            updateNav(activeNode);
+
+            // actually load the page if it hasn't been loaded
+            if (promise == null) {
+                promise = $.get(path);        
+            }
+
+            // set page content when the page is loaded
+            promise.done(function(data){ main_content_div.html(data); });
         };
     };
 
     var JenniferT2router = Backbone.Router.extend({
 
+        // you can see that the crow route takes an id so that 
+        // we can use the same renderer for each crow
         routes: {
             'home'          : 'renderHome',
             'crows'         : 'renderCrows',
-            'painted'       : 'renderPainted',
-            'goodwill'      : 'renderGoodwill',
-            'mending'       : 'renderMending',
-            'drummers'      : 'renderDrummers'
+            'crow/:id'      : 'renderCrow' 
         },
 
-        renderHome: getRenderer('/html/home.htm', $('nav li#home')),
+        renderHome: getRenderer('html/home.htm', $('.nav li#home')),
+        renderCrows: getRenderer('html/crows.htm', $('.nav li#crows')),
 
-        renderCrows: getRenderer('/html/crows.htm', $('nav li#crows')),
-
-        renderPainted: getRenderer('#', $('nav li#papers')),
-
-        renderGoodwill: getRenderer('#', $('nav li#papers')),
-
-        renderMending: getRenderer('#', $('nav li#papers')),
-
-        renderDrummers: getRenderer('#', $('nav li#papers')),
-
+        // crow renderers are cached so that they can be re-used
+        crowRenderers: {},
+        renderCrow: function(id) {
+            if (!this.crowRenderers[id]) {
+                this.crowRenderers[id] = getRenderer('html/crows/page' + id + '.htm', $('.nav li#crows'));
+            }
+            this.crowRenderers[id]()
+        }
     });
 
-    var router = new GinaAcademicRouter();
+    var router = new JenniferT2router();
 
+    // start the history part so that people can use the back button
     Backbone.history.start();
 
-    router.renderHome();
+    // start the navigator off at the home route
     router.navigate('#home');
+    
+    // actually render the home route
+    router.renderHome();
 });
